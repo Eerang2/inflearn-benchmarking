3de4d4b98a556a46d1d806b3c28333b16bf4afa0
jinwoo <dlwlsdn8022@gmail.com>
2025-01-31 18:09:44 +0900

    system.out.println 코드 제거

--- src/main/java/green/study/presentation/api/MainRestController.java
+++ src/main/java/green/study/presentation/api/MainRestController.java
@@ -4,7 +4,6 @@ import green.study.application.lecture.service.LectureService;
 import green.study.presentation.dto.LectureRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +18,8 @@ public class MainRestController {
 
     private final LectureService lectureService;
     @GetMapping("/recommend/free-backend-lectures")
-    public List<LectureRes> freeCourses(Model model) {
-        List<LectureRes> freeLectures = lectureService.getFreeLectures();
-        System.out.println(freeLectures.get(0).getImagePath());
-        System.out.println(freeLectures.get(0).getImageUniqueName());
-        return freeLectures;
+    public List<LectureRes> freeCourses() {
+        return lectureService.getFreeLectures();
 
     }
 }
